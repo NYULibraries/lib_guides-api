173955eca99dd6a4897e255a0b836d8279715bad
Eric Griffis <eric.griffis@nyu.edu>
2018-01-12 13:32:59 -0500

    Use reason_phrase when available for error message; otherwise use json, but rescue parsing error

--- lib/lib_guides/api/error.rb
+++ lib/lib_guides/api/error.rb
@@ -5,11 +5,27 @@ module LibGuides
 
       def initialize(faraday_response)
         @faraday_response = faraday_response
-        @json_response = JSON.parse(@faraday_response.body)
         super(message)
       end
 
       def message
+        @message ||= reason_phrase || generate_message
+      end
+
+      def reason_phrase
+        return unless faraday_response.reason_phrase
+        return unless faraday_response.reason_phrase.strip.length > 0
+        faraday_response.reason_phrase
+      end
+
+      def generate_message
+        @json_response = JSON.parse(@faraday_response.body)
+        generate_message_from_json
+      rescue JSON::ParserError => e
+        @faraday_response.body
+      end
+
+      def generate_message_from_json
         "#{json_response["error"]}: #{json_response["error_description"]}"
       end
     end
