7b49014ac190811f8e10c98c4c343305f8f7c5db
Eric Griffis <eric.griffis@nyu.edu>
2018-01-12 15:46:30 -0500

    Add pending for tests

---
+++ spec/lib/lib_guides/api/error_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'lib_guides/api'
+
+describe LibGuides::API::Error do
+  describe "message" do
+    pending
+  end
+
+  describe "reason_phrase" do
+    pending
+  end
+
+  describe "generate_message" do
+    pending
+  end
+
+  describe "generate_message_from_json" do
+    pending
+  end
+end
